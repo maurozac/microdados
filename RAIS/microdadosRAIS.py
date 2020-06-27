0091de2e1c672d7e66aefa87cbbc1cef26fad712
Mauro Zac <tapirus.terrestris@gmail.com>
2020-06-27 09:41:26 -0300

    Create microdadosRAIS.py

---
+++ RAIS/microdadosRAIS.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+RAIS
+
+ftp://ftp.mtps.gov.br/pdet/microdados/RAIS/
+
+algumas boas refs:
+
+https://github.com/guilhermejacob/guilhermejacob.github.io/blob/master/scripts/mtps.R
+https://github.com/rdahis/clean_RAIS
+
+"""
+
+__version__ = "1.0"
+__author__ = "Mauro Zackiewicz"   # codigo
+__email__ = "maurozac@gmail.com"
+__copyright__ = "Copyright 2020"
+__license__ = "New BSD License"
+__status__ = "Experimental"
+
+
+
+import json
+
+import pandas as pd
+
